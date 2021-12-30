0c5c85a0396082790095c3b2e8b19a418183bd1b
Renato Lima <renato.lima@bemobi.com.br>
2021-12-29 21:54:49 -0300

    Create a layer for debugging in VSCode (F5 -- F12 keys)

--- keyboards/annepro2/keymaps/natenho/keymap.c
+++ keyboards/annepro2/keymaps/natenho/keymap.c
@@ -3,11 +3,18 @@
 #include "qmk_ap2_led.h"
 #include "config.h"
 
-enum anne_pro_layers {
+enum anne_pro_layers
+{
   _BASE_LAYER,
   _FN1_LAYER,
   _FN2_LAYER,
+  _DEBUG_LAYER
 };
+
+const annepro2Led_t COLOR_FN1   = {.p.red = 0x00, .p.green = 0xff, .p.blue = 0x00, .p.alpha = 0xff};
+const annepro2Led_t COLOR_FN2   = {.p.red = 0x00, .p.green = 0x00, .p.blue = 0xff, .p.alpha = 0xff};
+const annepro2Led_t COLOR_DEBUG = {.p.red = 0x00, .p.green = 0xff, .p.blue = 0x00, .p.alpha = 0xff};
+const annepro2Led_t COLOR_BLACK = {.p.red = 0x00, .p.green = 0x00, .p.blue = 0x00, .p.alpha = 0xff};
 
 // Key symbols are based on QMK. Use them to remap your keyboard
 /*
@@ -36,18 +43,59 @@ enum anne_pro_layers {
 * |       |       |       |                                 |       |  LEFT | DOWN  | RIGHT |
 * \-----------------------------------------------------------------------------------------/
 */
- const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_BASE_LAYER] = KEYMAP( /* Base */
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    MO(_FN1_LAYER), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(_FN1_LAYER,KC_LEFT), LT(_FN2_LAYER,KC_DOWN), RCTL_T(KC_RGHT)
-),
-/*
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE_LAYER] = KEYMAP(/* Base */
+                           KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+                           KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+                           MO(_FN1_LAYER), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+                           KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
+                           KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(_FN1_LAYER, KC_LEFT), LT(_FN2_LAYER, KC_DOWN), RCTL_T(KC_RGHT)),
+    /*
 * Layer _FN1_LAYER
 * ,-----------------------------------------------------------------------------------------.
 * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |    | HOME | END |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |V-UP |V-DWN|MUTE |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  DEBUG  | Ctrl|
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+    [_FN1_LAYER] = KEYMAP(/* Base */
+                          KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+                          _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END, _______,
+                          _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
+                          _______, _______, _______, KC_CALCULATOR, _______, _______, _______, _______, _______, KC_INS, KC_DEL, _______,
+                          _______, _______, _______, _______, _______, _______, TG(_DEBUG_LAYER), _______),
+    /*
+* Layer _FN2_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* |     | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |BREAK | HOME | END |  \   |
+* |-----------------------------------------------------------------------------------------+
+* | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |V-DWN|V-UP |MUTE |  v  |  b  |  n  |  m  |PREV_TRACK|NEXT_TRACK| DEL |Shift |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |     MEDIA PLAY/PAUSE            |  Alt  |  FN1  |  FN2  | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+    [_FN2_LAYER] = KEYMAP(/* Base */
+                          _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, _______, _______,
+                          MO(_FN2_LAYER), _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, KC_PAUSE, KC_HOME, KC_END, _______,
+                          _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
+                          _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, _______, _______,
+                          _______, _______, _______, KC_MEDIA_PLAY_PAUSE, _______, MO(_FN1_LAYER), MO(_FN2_LAYER), _______),
+    /*
+* Layer _DEBUG_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* | ESC |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  Bksp     |
 * |-----------------------------------------------------------------------------------------+
 * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |    | HOME | END |   \    |
 * |-----------------------------------------------------------------------------------------+
@@ -59,102 +107,49 @@ enum anne_pro_layers {
 * \-----------------------------------------------------------------------------------------/
 *
 */
- [_FN1_LAYER] = KEYMAP( /* Base */
-    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-    KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_CALCULATOR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN2_LAYER), KC_TRNS
-),
-/*
-* Layer _FN2_LAYER
-* ,-----------------------------------------------------------------------------------------.
-* |  ~  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |    Bksp   |
-* |-----------------------------------------------------------------------------------------+
-* | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |BREAK | HOME | END |  \   |
-* |-----------------------------------------------------------------------------------------+
-* | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
-* |-----------------------------------------------------------------------------------------+
-* | Shift      |V-UP |V-DWN|MUTE |  v  |  b  |  n  |  m  |PREV_TRACK|NEXT_TRACK| DEL |Shift |
-* |-----------------------------------------------------------------------------------------+
-* | Ctrl  |  L1   |  Alt  |     MEDIA PLAY/PAUSE            |  Alt  |  FN1  |  FN2  | Ctrl  |
-* \-----------------------------------------------------------------------------------------/
-*
-*/
- [_FN2_LAYER] = KEYMAP( /* Base */
-    KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_TRNS, KC_TRNS,
-    MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUSE, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
-    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_MEDIA_PLAY_PAUSE, KC_TRNS, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_TRNS
- ),
+    [_DEBUG_LAYER] = KEYMAP(/* Base */
+                            KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
+                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                            _______, _______, _______, _______, _______, _______, _______, _______),
 };
 const uint16_t keymaps_size = sizeof(keymaps);
 
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-}
+void matrix_init_user(void) {}
+void matrix_scan_user(void) {}
 
 // Code to run after initializing the keyboard
-void keyboard_post_init_user(void) {
-    // Here are two common functions that you can use. For more LED functions, refer to the file "qmk_ap2_led.h"
+void keyboard_post_init_user(void)
+{
+  annepro2LedEnable();
 
-    // annepro2-shine disables LEDs by default. Uncomment this function to enable them at startup.
-    annepro2LedEnable();
-
-    // Additionally, it also chooses the first LED profile by default. Refer to the "profiles" array in main.c in
-    // annepro2-shine to see the order. Replace "i" with the index of your preferred profile. (i.e the RED profile is index 0)
-    // annepro2LedSetProfile(i);
+  // Additionally, it also chooses the first LED profile by default. Refer to the "profiles" array in main.c in
+  // annepro2-shine to see the order. Replace "i" with the index of your preferred profile. (i.e the RED profile is index 0)
+  // annepro2LedSetProfile(i);
 }
 
-layer_state_t layer_state_set_user(layer_state_t layer) {
-  switch(get_highest_layer(layer)) {
-    case _FN1_LAYER:
-      // Set the leds to green
-      annepro2LedSetForegroundColor(0x9A, 0xCD, 0x55);
-      break;
-    case _FN2_LAYER:
-      // Set the leds to blue
-      annepro2LedSetForegroundColor(0x5F, 0x9E, 0xA0);
-      break;
-    default:
-      // Reset back to the current profile
-      annepro2LedResetForegroundColor();
-      break;
+layer_state_t layer_state_set_user(layer_state_t layer)
+{
+  switch (get_highest_layer(layer))
+  {
+  case _FN1_LAYER:
+    annepro2LedMaskSetMono(COLOR_FN1);
+    break;
+  case _FN2_LAYER:
+    annepro2LedMaskSetMono(COLOR_FN2);
+    break;
+  case _DEBUG_LAYER:
+    annepro2LedMaskSetMono(COLOR_BLACK);
+    annepro2LedMaskSetKey(0, 5, COLOR_DEBUG); //F5
+    annepro2LedMaskSetKey(0, 9, COLOR_DEBUG); //F9
+    annepro2LedMaskSetKey(0, 10, COLOR_DEBUG); //F10
+    annepro2LedMaskSetKey(0, 11, COLOR_DEBUG); //F11
+    annepro2LedMaskSetKey(0, 12, COLOR_DEBUG); //F12
+    break;
+  default:
+    annepro2LedResetForegroundColor();
+    break;
   }
   return layer;
-}
-
-// The function to handle the caps lock logic
-// It's called after the capslock changes state or after entering layers 1 and 2.
-bool led_update_user(led_t leds) {
-  if (leds.caps_lock) {
-    // Set the caps-lock to red
-    const annepro2Led_t color = {
-        .p.red = 0xff,
-        .p.green = 0x00,
-        .p.blue = 0x00,
-        .p.alpha = 0xff
-    };
-
-    annepro2LedMaskSetKey(2, 0, color);
-    /* NOTE: Instead of colouring the capslock only, you can change the whole
-       keyboard with annepro2LedSetForegroundColor */
-  } else {
-    // Reset the capslock if there is no layer active
-    if(!layer_state_is(_FN1_LAYER) && !layer_state_is(_FN2_LAYER)) {
-      const annepro2Led_t color = {
-          .p.red = 0xff,
-          .p.green = 0x00,
-          .p.blue = 0x00,
-          .p.alpha = 0x00
-      };
-      annepro2LedMaskSetKey(2, 0, color);
-    }
-  }
-
-  return true;
 }
